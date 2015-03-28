adccdd63637673620030198ae7782c23e17a621a
Christophe Brun <cbrun@clever-age.com>
2015-03-28 12:36:13 +0100

    Init flask structure

---
+++ backend/README.rst
@@ -0,0 +1,18 @@
+DomoHack : Backend
+==================
+
+Installation
+------------
+
+* Clone repo
+* Install requirement
+
+.. code-block:: bash
+
+    pip install -f requirements.txt
+
+
+Architecture
+------------
+
+* Python + Framework Flask
---
+++ backend/config.py
@@ -0,0 +1,18 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = 'fHacktory C trop de la bombe'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config = {
+    'devel' : DevConfig,
+    'default' : DevConfig,
+}
